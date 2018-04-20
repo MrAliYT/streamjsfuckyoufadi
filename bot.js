f143ac4b5e6d5efa91db46e2d8c586cb725ed3f5
MrAliYT <38569896+MrAliYT@users.noreply.github.com>
2018-04-20 22:34:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,27 @@
 
-
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} !`);
-  client.user.setGame(`Welcome To The Gate of Hell !`,`www.twitch.tv/mrali__`);
+
+
+
+client.on("ready", () => {
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: 'إنْ لَمْ تَجِد لكْ حآقدْ إعلمْ أنْك إنسآن فآشِلْ',
+     details: `إنْ لَمْ تَجِد لكْ حآقدْ إعلمْ أنْك إنسآن فآشِلْ`,
+     url: 'http://twitch.tv/Streammingg',
+     state: `إنْ لَمْ تَجِد لكْ حآقدْ إعلمْ أنْك إنسآن فآشِلْ`,
+    application_id: '281376075802476544',
+     assets: {
+        small_image: `436971246245117954`,
+        small_text: ' Take This ! ' ,
+        large_image: `436971793421434881`,
+        large_text: `Welcome To The Gate Of Hell` }
+
+  }
+    });
 });
-
-
 
 client.login(process.env.BOT_TOKEN);
